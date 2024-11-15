d6c8430ee31167b6b8897d251111c8f8fb7ab29c
s-m-quadri <88645248+s-m-quadri@users.noreply.github.com>
2024-11-15 21:10:10 +0530

    Add credentials cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const productRouter= require('./routes/productRouter')
 const uploadRouter = require('./routes/upload')
 const app = express();
 
-app.use(cors())
+app.use(cors({credentials: true}))
 
 app.use(express.json());
 app.use(cookieParser());
